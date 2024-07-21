b2a1844100745d61f7382d4944a405629efafeb1
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-07-21 07:42:48 -0400

    Experimenting with charts

--- Nero Watch App/ContentView.swift
+++ Nero Watch App/ContentView.swift
@@ -1,26 +1,19 @@
-//
-//  ContentView.swift
-//  Nero Watch App
-//
-//  Created by Matthew Massicotte on 2024-04-15.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        TabView {
-            ParameterView(title: "Salinity", value: 35.0, unit: UnitDispersion.partsPerThousand)
-                .containerBackground(.blue, for: .tabView)
-            Text("B")
-                .containerBackground(.green, for: .tabView)
-            Text("C")
-                .containerBackground(.red, for: .tabView)
-        }
-        .tabViewStyle(.verticalPage)
-    }
+	var body: some View {
+		TabView {
+			ParameterView(title: "Salinity", value: 35.0, unit: UnitDispersion.partsPerThousand)
+				.containerBackground(.blue, for: .tabView)
+			Text("B")
+				.containerBackground(.green, for: .tabView)
+			Text("C")
+				.containerBackground(.red, for: .tabView)
+		}
+		.tabViewStyle(.verticalPage)
+	}
 }
 
 #Preview {
-    ContentView()
+	ContentView()
 }
--- Nero Watch App/ParameterView.swift
+++ Nero Watch App/ParameterView.swift
@@ -1,26 +1,62 @@
 import SwiftUI
+import Charts
 
 extension UnitDispersion {
-    public class var partsPerThousand: UnitDispersion {
-        UnitDispersion(symbol: "ppt", converter: UnitConverter())
-    }
+	public class var partsPerThousand: UnitDispersion {
+		UnitDispersion(symbol: "ppt", converter: UnitConverter())
+	}
+}
+
+struct ParameterDataPoint: Identifiable {
+	let value: Float
+	let date: Date
+	let id = UUID()
 }
 
 struct ParameterView: View {
-    let title: String
-    let value: Float
-    let unit: Dimension
+	let dataPoints: [ParameterDataPoint] = [
+		.init(value: 33.0, date: Date(timeIntervalSinceNow: -15)),
+		.init(value: 34.0, date: Date(timeIntervalSinceNow: -10)),
+		.init(value: 35.0, date: Date(timeIntervalSinceNow: -5)),
+		.init(value: 36.0, date: Date(timeIntervalSinceNow: 0)),
+	]
+	let title: String
+	let value: Float
+	let unit: Dimension
 
-    var body: some View {
-        VStack {
-            Text(title)
-            Text(String(value))
-                .font(.largeTitle)
-            Text(unit.symbol)
-        }
-    }
+	var body: some View {
+		VStack {
+			Text(title)
+			Text(String(value))
+				.font(.largeTitle)
+			Text(unit.symbol)
+			Chart(dataPoints) {
+				LineMark(
+					x: .value("Time", $0.date),
+					y: .value("Value", $0.value)
+				)
+				.foregroundStyle(Color.white)
+			}
+			.chartXAxis {
+//				AxisMarks(values: .stride(by: .month)) { value in
+//					AxisGridLine()
+//					AxisValueLabel(format: .dateTime.month(.defaultDigits))
+//				}
+			}
+			.chartYAxis {
+//				AxisMarks(values: [30, 31, 32, 33, 34, 35, 36, 37]) {
+//					AxisGridLine()
+//				}
+			}
+			.frame(height: 120)
+		}
+	}
 }
 
 #Preview {
-    ParameterView(title: "Salinity", value: 35.0, unit: UnitDispersion.partsPerThousand)
+	ParameterView(
+		title: "Salinity",
+		value: 35.0,
+		unit: UnitDispersion.partsPerThousand
+	)
 }
