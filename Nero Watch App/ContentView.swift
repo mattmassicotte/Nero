0f77aa34db1bc449b9d2f7b428c3e8cd1a36fe27
Matt <85322+mattmassicotte@users.noreply.github.com>
2024-10-27 07:59:01 -0400

    Use some more real parameters

--- Nero Watch App/ContentView.swift
+++ Nero Watch App/ContentView.swift
@@ -5,10 +5,11 @@ struct ContentView: View {
 		TabView {
 			ParameterView(title: "Salinity", value: 35.0, unit: UnitDispersion.partsPerThousand)
 				.containerBackground(.blue, for: .tabView)
-			Text("B")
-				.containerBackground(.green, for: .tabView)
-			Text("C")
-				.containerBackground(.red, for: .tabView)
+			ParameterView(title: "Nitrates", value: 30.8, unit: UnitDispersion.partsPerMillion)
+				.containerBackground(.purple, for: .tabView)
+			ParameterView(title: "Ammonia", value: 0.4, unit: UnitDispersion.partsPerMillion)
+				.containerBackground(.yellow, for: .tabView)
+
 		}
 		.tabViewStyle(.verticalPage)
 	}
